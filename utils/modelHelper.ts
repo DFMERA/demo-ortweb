730a6373dea70bcdb5ed7caa4c7a7c0e03c5bb00
dfmera <df.merau@gmail.com>
2022-09-27 15:36:47 -0500

    Change to wasm

--- utils/modelHelper.ts
+++ utils/modelHelper.ts
@@ -8,7 +8,7 @@ export async function runSqueezenetModel(preprocessedData: any): Promise<[any, n
   //https://onnxruntime.ai/docs/api/js/interfaces/InferenceSession.SessionOptions.html#graphOptimizationLevel
   const session = await ort.InferenceSession
                           .create('./_next/static/chunks/pages/squeezenet1_1.onnx', 
-                          { executionProviders: ['webgl'], graphOptimizationLevel: 'all' });
+                          { executionProviders: ['wasam'], graphOptimizationLevel: 'all' });
   console.log('Inference session created')
   // Run inference and get results.
   var [results, inferenceTime] =  await runInference(session, preprocessedData);
